7bcad1a008aab09d6792df462ead9877c1ebb250
Tymon Jasiński <tymon.jasinski010@gmail.com>
2024-10-20 21:33:54 +0200

    features 4 and 5

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,12 +7,29 @@ class TestCreateBankAccount(unittest.TestCase):
     def test_tworzenie_konta(self):
         imie = "Dariusz"
         nazwisko = "Januszewski"
-        pesel = "123456789"
-        pierwsze_konto = Konto(imie,naziwsko,pesel)
+        pesel = "12345678900"
+        pierwsze_konto = Konto(imie,nazwisko,pesel)
+
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko , "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel, pesel, "Pesel nie został zapisany!")
-        self.assertEqual(pierwsze_konto.pesel, pesel, "Niepoprawny pesel!")
+
+    def test_dodawanie_peselu(self):
+        imie = "Dariusz"
+        nazwisko = "Januszewski"
+        pesel = "12345678900"
+        konto = Konto(imie,nazwisko,pesel)
+
+        self.assertEqual(konto.pesel, pesel, "Pesel nie został zapisany!")
+
+    def test_dodawanie_kasy_kodem(self):
+        imie = "Dariusz"
+        nazwisko = "Januszewski"
+        pesel = "61345678900"
+        kod_rabatowy="PROM_AGF"
+        konto = Konto(imie,nazwisko,pesel,kod_rabatowy)
+        self.assertEqual(konto.saldo,50,"Kod nie został przyznany")
+
+
 
     #tutaj proszę dodawać nowe testy
