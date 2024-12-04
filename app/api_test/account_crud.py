76359de820c099c596e731abbb7592c39aa36475
Tymon Jasiński <tymon.jasinski010@gmail.com>
2024-12-04 01:15:34 +0100

    more Lint issues

--- app/api_test/account_crud.py
+++ app/api_test/account_crud.py
@@ -36,27 +36,27 @@ class TestApi(unittest.TestCase):
         self.assertEqual(r.status_code,409)
 
     def test_transfer_incoming(self):
-        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"incoming","amount":500})
+        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"incoming","amount":500})
         self.assertEqual(r.status_code,200)
     
     def test_transfer_outgoing(self):
         requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"incoming","amount":1000})
 
-        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"outgoing","amount":500})
+        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"outgoing","amount":500})
         self.assertEqual(r.status_code,200)
 
     def test_transfer_outgoing_express(self):
         requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"incoming","amount":1000})
 
-        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"express","amount":500})
+        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"express","amount":500})
         self.assertEqual(r.status_code,200)
 
     def test_transfer_outgoing_too_much(self):
         requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}",json={"type":"incoming","amount":100})
 
-        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"outgoing","amount":500})
+        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"outgoing","amount":500})
         self.assertEqual(r.status_code,422)
 
     def test_transfer_wrong_type(self):
-        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"none","amount":500})
+        r = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"none","amount":500})
         self.assertEqual(r.status_code,405)
