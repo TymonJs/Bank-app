3513951acc835c099eb7315e1af1f685d01badb2
Tymon Jasiński <tjasinski@INF.UG.EDU.PL>
2025-01-14 13:34:07 +0100

    feature_21 fix lint errors

--- app/api_performance_test/test_api_performance.py
+++ app/api_performance_test/test_api_performance.py
@@ -12,14 +12,14 @@ class TestCreateDeleteAccount(unittest.TestCase):
         for i in range(self.tries):
             rp = requests.post("http://127.0.0.1:5000/api/accounts",json=self.data,timeout=self.timeout)
             self.assertEqual(rp.status_code,201)
-            rd = requests.delete(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}",timeout=self.timeout)
+            rd = requests.delete(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}",timeout=self.timeout)
             self.assertEqual(rd.status_code,200)
             
 
     def test_ksiegowanie_przelewow(self):
         rp = requests.post("http://127.0.0.1:5000/api/accounts",json=self.data)
         for i in range(self.tries):
-            rt = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}/transfer",json={"type":"incoming","amount":100},timeout=self.timeout)
+            rt = requests.post(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}/transfer",json={"type":"incoming","amount":100},timeout=self.timeout)
             self.assertEqual(rt.status_code,200)
         
-        rd = requests.delete(f"http://127.0.0.1:5000/api/accounts/{self.data["pesel"]}")
+        rd = requests.delete(f"http://127.0.0.1:5000/api/accounts/{self.data['pesel']}")
